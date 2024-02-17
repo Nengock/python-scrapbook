935ae485062adc27eee0feb35047ff8f05606786
Wayan P <nengock@yahoo.com>
2024-02-17 21:07:02 +0700

    feat: setup testing

---
+++ 00-setup/env-testing.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import tensorflow as tf
+import torch
+
+print(f'Numpy: {np.__version__}')
+print(f'Pandas: {pd.__version__}')
+print(f'Scikit-learn: {sklearn.__version__}')
+print(f'Tensorflow: {tf.__version__}')
+print(f'Torch: {torch.__version__}')
